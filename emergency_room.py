ca2acdff0aacec5fa2826e6514b1b1039790b792
MigueM531 <MigueM531>
2025-10-28 22:44:25 -0500

    Implementar registro y programación de cirugía para pacientes en el quirófano

--- emergency_room.py
+++ emergency_room.py
@@ -1,12 +1,145 @@
+from Arbol import *
+from queue import *
+
 class Paciente:
     orden = 0
-    def __init__(self, id, codigo_paciente, nivel_emergencia):
+    def __init__(self, id, nombre, nivel_emergencia):
         Paciente.orden += 1
         self.id = id
-        self.codigo_paciente = codigo_paciente
+        self.nombre = nombre
         self.nivel_emergencia: int = nivel_emergencia
         self.orden_llegada = Paciente.orden
 
-    def __str__(self):
-        return f"{self.id}, {self.orden_llegada}"
+    def __lt__(self, other):
+        if self.nivel_emergencia == other.nivel_emergencia:
+            return self.orden_llegada < other.orden_llegada
+        return self.nivel_emergencia < other.nivel_emergencia
 
+    def __str__(self):
+        return f"{self.nombre} [PRIORIDAD: {self.nivel_emergencia} - LLEGADA: {self.orden_llegada}]"
+
+
+class Quirofano:
+    def __init__(self):
+        self.root = None
+
+
+    def registrar_paciente(self, paciente):
+        nuevo_nodo = BinaryTreeNode(paciente)
+
+        if self.root is None:
+            self.root = nuevo_nodo
+            return
+
+        q = Queue()
+        q.enqueue(self.root)
+
+        while not q.is_empty():
+            actual = q.dequeue()
+
+            if actual.leftchild is None:
+                actual.leftchild = nuevo_nodo
+                nuevo_nodo.parent = actual
+                self.heap_up(nuevo_nodo)
+                return
+
+            elif actual.rightchild is None:
+                actual.rightchild = nuevo_nodo
+                nuevo_nodo.parent = actual
+                self.heap_up(nuevo_nodo)
+                return
+
+            q.enqueue(actual.leftchild)
+            q.enqueue(actual.rightchild)
+
+
+    def heap_up(self, nodo):
+        while nodo.parent and nodo.data < nodo.parent.data:
+            nodo.data, nodo.parent.data = nodo.parent.data, nodo.data
+            nodo = nodo.parent
+
+
+    def consultar_siguiente(self):
+        if self.root is not None:
+            return self.root.data
+        
+    
+    def programar_cirugia(self):
+        # 1️⃣ Si no hay pacientes
+        if self.root is None:
+            return None
+
+        paciente_siguiente = self.root.data
+
+        if self.root.leftchild is None and self.root.rightchild is None:
+            self.root = None
+            return paciente_siguiente
+
+        q = Queue()
+        q.enqueue(self.root)
+        ultimo = None
+        padre_ultimo = None
+
+        while not q.is_empty():
+            actual = q.dequeue()
+            if actual.leftchild:
+                padre_ultimo = actual
+                q.enqueue(actual.leftchild)
+            if actual.rightchild:
+                padre_ultimo = actual
+                q.enqueue(actual.rightchild)
+            ultimo = actual
+
+
+        self.root.data = ultimo.data
+
+        if padre_ultimo.rightchild == ultimo:
+            padre_ultimo.rightchild = None
+        else:
+            padre_ultimo.leftchild = None
+
+        self.heap_down(self.root)
+
+        print(f"{paciente_siguiente.nombre} será llevado a cirugía.")
+        return paciente_siguiente
+
+
+    def heap_down(self, nodo):
+        if nodo is None:
+            return
+
+        menor = nodo
+
+        if nodo.leftchild and nodo.leftchild.data < menor.data:
+            menor = nodo.leftchild
+        if nodo.rightchild and nodo.rightchild.data < menor.data:
+            menor = nodo.rightchild
+
+        if menor != nodo:
+            nodo.data, menor.data = menor.data, nodo.data
+            self.heap_down(menor)  # llamada recursiva
+
+
+
+
+quirofano = Quirofano()
+
+quirofano.registrar_paciente(Paciente(1, "Ana", 2))
+quirofano.registrar_paciente(Paciente(2, "Luis", 1))
+quirofano.registrar_paciente(Paciente(3, "Marta", 4))
+quirofano.registrar_paciente(Paciente(4, "Carlos", 1))
+quirofano.registrar_paciente(Paciente(5, "Andres", 3))
+
+print("\n=== Árbol de pacientes ===")
+printTree(quirofano.root)
+
+print(f"\nSIGUIENTE A QUIROFANO: {quirofano.consultar_siguiente()}")
+
+print("\n=== Paciente en cirugía ===")
+atendido = quirofano.programar_cirugia()
+print(f"Se atendió a: {atendido}")
+
+print("\n=== Árbol de pacientes ===")
+printTree(quirofano.root)
+
+print(f"\nSIGUIENTE A QUIROFANO: {quirofano.consultar_siguiente()}")
